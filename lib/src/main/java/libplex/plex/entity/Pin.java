64786ad02ae2f37ac10557e48f043a991fab99c7
Gianluca <coviello.gl@gmail.com>
2022-02-09 12:52:31 +0100

    Refactored Pin class

--- lib/src/main/java/libplex/plex/entity/Pin.java
+++ lib/src/main/java/libplex/plex/entity/Pin.java
@@ -10,84 +10,45 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.NONE)
 public class Pin {
-    private Integer id;
-    private String code;
-    private Date expiresAt;
-    private String clientIdentifier;
-    private Boolean trusted;
-    private Integer userId;
-    private String authToken;
-    private String auth_Token;
+    @XmlElement private Integer id;
+    @XmlElement private String code;
+    @XmlElement(name = "expires-at") private Date expiresAt;
+    @XmlElement(name = "client-identifier") private String clientIdentifier;
+    @XmlElement private Boolean trusted;
+    @XmlElement(name = "user-id") private Integer userId;
+    @XmlElement(name = "auth-token") private String authToken;
+    @XmlElement(name = "auth_token") private String auth_Token;
 
-    @XmlElement(name = "user-id")
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    @XmlElement
     public Integer getId() {
         return id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @XmlElement
     public String getCode() {
         return code;
     }
 
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    @XmlElement(name = "expires-at")
     public Date getExpiresAt() {
         return expiresAt;
     }
 
-    public void setExpiresAt(Date expiresAt) {
-        this.expiresAt = expiresAt;
-    }
-
-    @XmlElement(name = "client-identifier")
     public String getClientIdentifier() {
         return clientIdentifier;
     }
 
-    public void setClientIdentifier(String clientIdentifier) {
-        this.clientIdentifier = clientIdentifier;
-    }
-
-    @XmlElement
-    public Boolean isTrusted() {
+    public Boolean getTrusted() {
         return trusted;
     }
 
-    public void setTrusted(Boolean trusted) {
-        this.trusted = trusted;
+    public Integer getUserId() {
+        return userId;
     }
 
-    @XmlElement(name = "auth-token")
     public String getAuthToken() {
         return authToken;
     }
 
-    public void setAuthToken(String authToken) {
-        this.authToken = authToken;
-    }
-
-    @XmlElement(name = "auth_token")
     public String getAuth_Token() {
         return auth_Token;
     }
 
-    public void setAuth_Token(String auth_Token) {
-        this.auth_Token = auth_Token;
-    }
 }
