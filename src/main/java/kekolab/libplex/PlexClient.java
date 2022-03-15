4222184ad1878979204af389b4fa96a564e28667
Gianluca <coviello.gl@gmail.com>
2022-03-15 15:35:33 +0100

    rewriting the tests

--- src/main/java/kekolab/libplex/PlexClient.java
+++ src/main/java/kekolab/libplex/PlexClient.java
@@ -1,20 +1,25 @@
 package kekolab.libplex;
 
+import jakarta.ws.rs.core.UriBuilder;
+import kekolab.libplex.entity.PlexItem;
+import kekolab.libplex.entity.PlexMediaServer;
+
 public class PlexClient extends PlexBaseClient {
     public PlexClient(String token, String product, String version, String clientIdentifier, String platform,
             String platformVersion, String device, String deviceName, PlexProvides[] provides) {
         super(token, product, version, clientIdentifier, platform, platformVersion, device, deviceName, provides);
     }
+
 //    public Servers servers() {
 //        return new Servers(this, UriBuilder.fromUri("https://plex.tv/pms/servers.xml")
 //                .build());
 //    }
 //
-//    public ServerContent localServer(String host, int port) {
-//        return new ServerContent(this, UriBuilder.newInstance()
-//                .scheme("http")
-//                .host(host)
-//                .port(port)
-//                .build());
-//    }
+    public PlexMediaServer localServer(String host, int port) {
+        return new PlexItem.Builder<PlexMediaServer>(this, UriBuilder.newInstance()
+                .scheme("http")
+                .host(host)
+                .port(port)
+                .build()).build(PlexMediaServer.class);
+    }
 }
